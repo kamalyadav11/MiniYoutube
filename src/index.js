51474ce1fc357d802dec2ef65a2caf5dda4450bd
kamal11 <kyzx11@gmail.com>
2018-08-25 00:20:35 +0530

    rendered VideoList with Image

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
 
 const API_KEY = "AIzaSyBOUvaqNHMacCc9V0bvbinL_woM6gW3Aew";
 
@@ -12,6 +13,7 @@ class App extends React.Component {
     this.state = { videos: [] };
 
     YTSearch({ key: API_KEY, term: "surfboards" }, data => {
+      //This request takes some time basically a async request
       this.setState({ videos: data });
     });
   }
@@ -20,6 +22,7 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
