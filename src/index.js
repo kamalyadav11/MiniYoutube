ac90aa25c46f3521dda1eae39a09b6edd726ea20
kamal11 <kyzx11@gmail.com>
2018-08-24 23:28:23 +0530

    Initialised youtube API_KEY

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/SearchBar";
 
 const API_KEY = "AIzaSyBOUvaqNHMacCc9V0bvbinL_woM6gW3Aew";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: "surfboards" }, data => {
+      this.setState({ videos: data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
